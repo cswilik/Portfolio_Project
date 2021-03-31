856e378bf5852f8241be903dcb9e1be729cbee2b
CSWILIK <cswilik@gmail.com>
2021-03-31 13:07:14 -0400

    add portfolio react project

---
+++ src/pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+
+function Homepage() {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <Header/>
+            </div>
+            <h1>Hello this is homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage;
