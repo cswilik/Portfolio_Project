f66a55f1bc5c765d2fd47f075605079a911e473e
CSWILIK <cswilik@gmail.com>
2021-04-14 12:46:39 -0400

    updating projects list

--- src/projects.js
+++ src/projects.js
@@ -1,17 +1,23 @@
+import Interior from './interior.png';
+
 const projectList = [
     {
         projectName: 'Interior',
-        image: 'url_here',
-        gitHub: 'gitHub link',
-        description: 'description here',
-        walkthrough: 'youtube link'
+        image: Interior,
+        gitHubFrontend: 'https://github.com/cswilik/interior-frontend',
+        gitHubBackend: 'https://github.com/cswilik/interior-backend',
+        stack: ['Rails', 'React Hooks', 'Redux'],
+        description: 'A form of travel app that allows users to track and share their visits to America’s National Parks.',
+        demo: 'https://www.youtube.com/watch?v=NdhnFYzH2DM'
     },
     {
         projectName: 'Gaias Sandbox',
         image: 'url_here',
-        gitHub: 'gitHub link',
+        gitHubFrontend: 'https://github.com/cswilik/gaias-sandbox-frontend',
+        gitHubBackend: 'https://github.com/cswilik/gaias-sandbox-backend',
         description: 'description here',
-        walkthrough: 'youtube link'
+        link: 'https://gaias-sandbox.herokuapp.com/',
+        demo: 'https://www.youtube.com/watch?v=DVUo4A6czbg'
     },
     {
         projectName: 'Grubdex',
