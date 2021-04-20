c031455e51780c09a18c7ff47e302eb9542a686c
CSWILIK <cswilik@gmail.com>
2021-04-20 11:45:23 -0400

    updating about me section

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,11 @@ import React from 'react';
 
 function Homepage() {
     return (
-        <div><h1>Hello its me</h1></div>
+        <div className="About-me"><h2>Hi! I'm Chelsey</h2>
+        <p>Full stack engineer new to the field of tech. I'm a huge proponent of collaboration and  communication. 
+            My skill sets include: Rails, Javascript, React with Hooks, Redux. I don't know what else to say here,
+            but I am trying to make a little paragraph.</p>
+        </div>
     )
 }
 
