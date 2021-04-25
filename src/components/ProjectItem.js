e8ea90be9d7149d3a2206340b98936cb8ffd4850
CSWILIK <cswilik@gmail.com>
2021-04-25 18:26:58 -0500

    minor adjustments

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 function ProjectItem({project}) {
     return (<div >
     <h4 className="text-font">{project.projectName}</h4>
-    <a href={project.githubFrontend}>Github Frontend</a>
-    <img className="project-img" src={project.image}/>
+    <a target="_blank" rel="noreferrer" href={project.gitHubFrontend}>Github Frontend</a>
+    
+
+    <img className="project-img" src={project.image} alt="project screenshot"/>
 
     <p>{project.description}</p>
     </div>
